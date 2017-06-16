a087ebd18e9d74151d46df1faf168ee28b2adcc3
projectku <piter@lipat.co.id>
2017-06-16 16:45:04 +0700

    Add One Signal

--- www/js/app.js
+++ www/js/app.js
@@ -3,14 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ionic-ratings','ui.calendar','auth0','ionic-zoom-view'])
+angular.module('starter', ['ngCordova','ionic','ionic-ratings','ui.calendar','auth0','ionic-zoom-view'])
 
 .run(function($ionicPlatform,$location,auth,$rootScope,StorageService) 
 {
   auth.hookEvents();
   $ionicPlatform.ready(function() 
   {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) 
+    {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -20,9 +21,26 @@ angular.module('starter', ['ionic','ionic-ratings','ui.calendar','auth0','ionic-
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if(window.StatusBar) 
+    {
       StatusBar.styleDefault();
     }
+    var notificationOpenedCallback = function(jsonData) 
+    {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+
+    window.plugins.OneSignal.startInit("bc2e8fd6-c7df-4d89-b32f-2df294be0f1a","282260926521")
+                  .handleNotificationOpened(notificationOpenedCallback)
+                  .endInit();
+    // var notificationOpenedCallback = function(jsonData) 
+    //   {
+    //     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    //   };
+
+    //   window.plugins.OneSignal.startInit("a291df49-653d-41ff-858d-e36513440760", "943983549601")
+    //                 .handleNotificationOpened(notificationOpenedCallback)
+    //                 .endInit();
   });
   $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) 
   {
